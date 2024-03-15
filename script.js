0d0e3af3eca03840a32e6fea052aa2d660601b91
Tanjidul-Islam <0tanjid0@gmail.com>
2024-03-15 15:15:18 +0600

    first commit

---
+++ script.js
@@ -0,0 +1,38 @@
+document.addEventListener("DOMContentLoaded", function () {
+  const navBtn = document.querySelector("#navBtn");
+  const navItems = document.querySelector(".navItems");
+  const navBar = document.querySelector(".navbar");
+
+  navBtn.addEventListener("click", () => {
+    navItems.classList.toggle("expand");
+    navBar.classList.toggle("navOnExpand");
+  });
+
+  // Navbar details
+  var details = document.querySelectorAll("details");
+  details.forEach(function (detail) {
+    detail.addEventListener("click", function () {
+      details.forEach(function (otherDetail) {
+        if (otherDetail !== detail && otherDetail.open) {
+          otherDetail.open = false;
+        }
+      });
+    });
+  });
+
+  // Footer
+  const footerTitles = document.querySelectorAll(".footerTitle");
+  const footerLinks = document.querySelectorAll(".footerLinks");
+
+  footerTitles.forEach((title, index) => {
+    title.addEventListener("click", () => {
+      footerLinks.forEach((link, i) => {
+        if (i === index) {
+          link.classList.toggle("hideShowLinks");
+        } else {
+          link.classList.remove("hideShowLinks");
+        }
+      });
+    });
+  });
+});
